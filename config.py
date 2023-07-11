88a8136b033a29d4503d3422ac07f6ec0f2a035c
1liochka1 <ila00001120@gmail.com>
2023-07-12 00:10:23 +0400

    add final nft

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 """
 
-all (все 5) (number не трогаем) / exact (какую то конкретную(надо cразу подставить number))
+all (все 5) (number не трогаем) / exact (какую то конкретную(надо cразу подставить number)) / final (последняя после 5 шт)
 delay - делей от и до
 amount_wallets_in_batch - количество кошельков в одновременном запуске
 shuffle - перемешка кошельков 1 вкл 0 выкл
 """
-mode = 'exact'
+mode = ''
 number = 0  # от 0 - 1 - 2 - 3 - 4
 delay = (1, 100)
 amount_wallets_in_batch = 1
